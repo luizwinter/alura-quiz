4eb06f24795184879a6ebff1c9902263ef3b9cf9
Luiz Winter Jr <77393235+luizwinter@users.noreply.github.com>
2021-01-27 10:34:43 -0300

    ajustando o logo

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import QuizLogo from '../src/components/QuizLogo'
+
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -28,6 +30,7 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+       <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
